e9b1fec89afb31a44540aeb75845fb3d53e61e19
David M <PieKing1215@users.noreply.github.com>
2022-07-28 01:31:59 -0400

    Fix publish_all_maven_local.sh

--- publish_all_maven_local.sh
+++ publish_all_maven_local.sh
@@ -1,4 +1,4 @@
-TERM=cygwin ./gradlew crossversion:publishToMavenLocal
-TERM=cygwin ./gradlew mc1.16:publishToMavenLocal -PdisableAllBut=mc1.16 --info
-TERM=cygwin ./gradlew mc1.18:publishToMavenLocal -PdisableAllBut=mc1.18 --info
-TERM=cygwin ./gradlew mc1.19:publishToMavenLocal -PdisableAllBut=mc1.19 --info
+TERM=cygwin ./gradlew crossversion:publishAllToMavenLocal
+TERM=cygwin ./gradlew mc1.16:publishAllToMavenLocal -PdisableAllBut=mc1.16
+TERM=cygwin ./gradlew mc1.18:publishAllToMavenLocal -PdisableAllBut=mc1.18
+TERM=cygwin ./gradlew mc1.19:publishAllToMavenLocal -PdisableAllBut=mc1.19
