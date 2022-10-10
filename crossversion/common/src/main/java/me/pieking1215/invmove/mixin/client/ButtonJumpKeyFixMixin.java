5a074536a6073144b592432c33ce1f589b2819fd
David M <PieKing1215@users.noreply.github.com>
2022-10-10 19:55:14 -0400

    Fix quick villager trading
    Fixes gh-20

--- crossversion/common/src/main/java/me/pieking1215/invmove/mixin/client/ButtonJumpKeyFixMixin.java
+++ crossversion/common/src/main/java/me/pieking1215/invmove/mixin/client/ButtonJumpKeyFixMixin.java
@@ -4,6 +4,7 @@ import me.pieking1215.invmove.InvMove;
 import me.pieking1215.invmove.InvMoveConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractButton;
+import net.minecraft.client.gui.screens.inventory.MerchantScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,8 +23,12 @@ public class ButtonJumpKeyFixMixin {
         // but if the player can jump in this inventory we cancel that
         // (fix for https://github.com/PieKing1215/InvMove/issues/2)
         if (i == 32 && InvMoveConfig.GENERAL.ENABLED.get() && InvMoveConfig.MOVEMENT.ENABLED.get() && InvMoveConfig.MOVEMENT.JUMP.get()) {
-            if (InvMove.instance().allowMovementInScreen(Minecraft.getInstance().screen)) {
-                cir.setReturnValue(false);
+            // TODO: consider doing this a better way
+            if(!((AbstractButton)(Object)this instanceof MerchantScreen.TradeOfferButton)) {
+                System.out.println(this + " " + i + " " + j + " " + k);
+                if (InvMove.instance().allowMovementInScreen(Minecraft.getInstance().screen)) {
+                    cir.setReturnValue(false);
+                }
             }
         }
     }
