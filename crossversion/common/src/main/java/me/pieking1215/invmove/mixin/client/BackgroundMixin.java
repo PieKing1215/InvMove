b64de7e723e1d71ddec770680c281a51b4c89c39
David M <PieKing1215@users.noreply.github.com>
2023-03-15 17:01:13 -0400

    Tweak BackgroundMixin to work in 1.19.4

--- crossversion/common/src/main/java/me/pieking1215/invmove/mixin/client/BackgroundMixin.java
+++ crossversion/common/src/main/java/me/pieking1215/invmove/mixin/client/BackgroundMixin.java
@@ -5,14 +5,16 @@ import me.pieking1215.invmove.InvMove;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Screen.class)
 public class BackgroundMixin {
+    @Group
     @Inject(
             method = "renderBackground(Lcom/mojang/blaze3d/vertex/PoseStack;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;renderBackground(Lcom/mojang/blaze3d/vertex/PoseStack;I)V"),
+            at = @At(value = "HEAD"),
             cancellable = true
     )
     private void onRenderBackground(PoseStack poseStack, CallbackInfo ci){
