8b1d742065af899e8cb9434beb14d3067e841659
PieKing1215 <PieKing1215@users.noreply.github.com>
2025-01-15 16:54:13 -0500

    Workaround for KeyMapping.compareTo NPE

---
+++ common/src/main/java/me/pieking1215/invmove/mixin/client/KeyMappingCompareMixin.java
@@ -0,0 +1,24 @@
+package me.pieking1215.invmove.mixin.client;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.KeyMapping;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Map;
+
+@Mixin(KeyMapping.class)
+public class KeyMappingCompareMixin {
+    @WrapOperation(
+            method = "compareTo(Lnet/minecraft/client/KeyMapping;)I",
+            at = @At(value = "INVOKE", target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;")
+    )
+    private Object safeCompare(Map<String, Integer> instance, Object key, Operation<Integer> original) {
+        // the vanilla compareTo function doesn't check that CATEGORY_SORT_ORDER actually contains the category
+        // which can result in NPE in Integer.compareTo when we sort the keys stream in InvMoveConfig
+        // (see https://github.com/PieKing1215/InvMove/issues/68#issuecomment-2593932606)
+        Object order = original.call(instance, key);
+        return order == null ? 0 : order;
+    }
+}
